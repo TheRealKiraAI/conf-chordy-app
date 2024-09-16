bb02c1b00fb0af1782e63f8d8d87c91be80f7916
KiraCorbett <kiracorbett00@gmail.com>
2024-09-16 11:24:34 -0700

    removed comments

--- app/utils/usePitchDetection.js
+++ app/utils/usePitchDetection.js
@@ -1,20 +1,12 @@
 import { useEffect, useState } from "react";
 import { freqToMidi, startAudioContext } from "./audioUtils";
 
-/*
- * name: pitchDetection is a function that takes in a model path, audioContext, and stream, and returns a pitchDetection object
- * input: modelPath, audioContext, stream
- */
 const usePitchDetection = () => {
   const [detectedNote, setDetectedNote] = useState("");
   let audioContext;
   let pitch;
   let stream;
 
-  /*
-   * this hooks runs once when the component mounts.
-   * setups up audio, requests user's microphone, and calls startPitch WHEN setup is complete
-   */
   useEffect(() => {
     const setup = async () => {
       audioContext = new AudioContext();
@@ -27,25 +19,12 @@ const usePitchDetection = () => {
     setup();
   }, []);
 
-  /*
-   *  name: startPitch is a function that takes in the stream and audioContext and calls the pitchDetection function
-   * input: stream, audioContext
-   */
   // ----------> start pitch detection <----------
 
-  /*
-   * callback that triggers the getPitch function WHEN the model is loaded.
-   */
   const modelLoaded = () => {
     getPitch();
   };
 
-  /*
-   * name: getPitch continously retrieves audio from pitch.getPitch
-   * if frequency detected, it converts to a MIDI number and maps it to a note in the scale array.
-   * updates the detectedNote state
-   * recursive to continously check for pitch changes.
-   */
   const getPitch = () => {
     pitch.getPitch((err, frequency) => {
       if (frequency) {
@@ -57,7 +36,7 @@ const usePitchDetection = () => {
     });
   };
 
-  return detectedNote; // hook returns detected musical note, allowing components to use the detectedNote
+  return detectedNote;
 };
 
 export default usePitchDetection;
